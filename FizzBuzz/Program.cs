f68bf46b923e7693ecf525d9286e65ed28e2d5e7
Harry Goodburn <Harry.Goodburn@softwire.com>
2021-01-12 16:07:52 +0000

    Added utilisation of enums to program

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -4,40 +4,70 @@ using System.Linq;
 
 namespace FizzBuzz
 {
+    [Flags]
+    public enum Instructions
+    {
+        None = 0b_0000_0000,
+        Fizz = 1,
+        Buzz = 2,
+        Bang = 4,
+        Bong = 8,
+        Fezz = 16,
+        Reverse = 32
+    }
+    
     internal class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Enter an upper bound for FizzBuzz:");
             var limit = Convert.ToInt32(Console.ReadLine());       // Upper bound on numbers to process, obtained from user
+            var rules = new Dictionary<int, Instructions>()
+            {
+                {3, Instructions.Fizz},
+                {5, Instructions.Buzz},
+                {7, Instructions.Bang},
+                {11, Instructions.Bong},
+                {13, Instructions.Fezz},
+                {17, Instructions.Reverse}
+            };
             
             for (var i = 1; i <= limit; i++)
             {
                 var response = "";
-                var words = new List<string>();           // Holds the string value that will eventually be output
+                var divisors = Instructions.None;           // Holds the string value that will eventually be output
+                var words = new List<string>();
+
+                for (var j = 0; j < rules.Count(); j++)
+                {
+                    if (i % rules.ElementAt(j).Key == 0)
+                    {
+                        divisors |= rules.ElementAt(j).Value;
+                    }
+                }
                 
-                if (i % 13 == 0)            // Carry out rule for Fezz first as the position of this respective to the other words is most crucial
+                if ((divisors & Instructions.Fezz) == Instructions.Fezz)            // Carry out rule for Fezz first as the position of this respective to the other words is most crucial
                 {
                     words.Add("Fezz");     // Words ended with commas so they can be separated ad have order reversed if necessary 
                 }
                 
-                if (i % 11 == 0)            // Other words added if required in relevant positions change
+                if ((divisors & Instructions.Bong) == Instructions.Bong)            // Other words added if required in relevant positions change
                 {
                     words.Add("Bong");
                 }
                 else
                 {
-                    if (i % 3 == 0)
+                    if ((divisors & Instructions.Fizz) == Instructions.Fizz)
                     {
                         words.Insert(0, "Fizz");
                     }
 
-                    if (i % 5 == 0)
+                    if ((divisors & Instructions.Buzz) == Instructions.Buzz)
                     {
                         words.Add("Buzz");
                     }
 
-                    if (i % 7 == 0)
+                    if ((divisors & Instructions.Bang) == Instructions.Bang)
                     {
                         words.Add("Bang");
                     }
@@ -50,7 +80,7 @@ namespace FizzBuzz
                 }
                 else                           // Otherwise we remove the commas from our output, reversing the order of words if necessary
                 {
-                    if (i % 17 == 0)
+                    if ((divisors & Instructions.Reverse) == Instructions.Reverse)
                     {
                         response = words.Aggregate("", (current, word) => word + current);
                     }
